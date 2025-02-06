49ecfbcba34afee427f664d297bde3dd70f472f5
YOUSSEF1806 <youssef1806@yahoo.fr>
2025-02-06 15:01:16 +0100

    Seam Carving Stage 4 of 6 Draft 3

--- src/main/kotlin/SeamProcessing.kt
+++ src/main/kotlin/SeamProcessing.kt
@@ -3,43 +3,58 @@ package com.youyou
 import com.youyou.model.NodeData
 import com.youyou.model.Pixel
 import com.youyou.model.Seam
-import java.util.*
 
-// add prevGraph: Map<Pixel, NodeData> as parameter to use as memorization path
-fun dijkstraEnergyPath(listEnergies: List<List<Double>>, source: Pixel): Map<Pixel, NodeData> {
-    val graph = mutableMapOf<Pixel, NodeData>()
-    val queue: PriorityQueue<Pair<Double, Pixel>> = PriorityQueue(compareBy { it.first })
-
-    queue.add(Pair(listEnergies[source.line][source.col],source))
-    while(queue.isNotEmpty()) {
-        val (currentEnergy, current) = queue.poll()
-        val adjacentPixels = nextAdjacentPixels(current.line, current.col)
-            .filter { it.line in listEnergies.indices && it.col in listEnergies[0].indices}
-        for (p in adjacentPixels) {
-            val pDistance = currentEnergy + listEnergies[p.line][p.col]
-            if(pDistance < (graph.getOrElse(p) { NodeData() }.energy)) {
-                graph[p] = NodeData(pDistance, current)
-                queue.add(Pair(pDistance, p))
-            }
-        }
-    }
-    return graph.toMap()
+fun processAndFindMinSeam(listEnergies: List<List<Double>>): Seam {
+    val graphNodes = createGraphNodes(listEnergies)
+    return getMinSeam(graphNodes)
 }
 
-fun backTrackGraph(graph: Map<Pixel, NodeData>, endIndex: Int): Seam {
-    val lastNode = graph.filterKeys { it.line == endIndex }.minBy { it.value.energy }
-    var current = lastNode.key
+fun getMinSeam(graphNodes: List<List<NodeData>>): Seam {
+    var (minSeamEnergy, current) = getMinSeamEnergyAndStart(graphNodes)
     val path = mutableListOf(current)
 
-    while (graph[current]?.parent != null) {
-        current = graph[current]?.parent!!
+    while (graphNodes[current.line][current.col].next != null) {
+        current = graphNodes[current.line][current.col].next!!
         path.add(current)
     }
-    return Seam(lastNode.value.energy, path.reversed())
+    return Seam(minSeamEnergy, path)
 }
 
-fun nextAdjacentPixels(line: Int, col: Int) = listOf(
-    Pixel(line + 1, col - 1),
-    Pixel(line + 1, col),
-    Pixel(line + 1, col + 1)
-)
+fun getMinSeamEnergyAndStart(graphNodes: List<List<NodeData>>): Pair<Double, Pixel> {
+    val minSeam = graphNodes.first().minBy { it.energy }
+    val index = graphNodes.first().indexOf(minSeam)
+    return Pair(minSeam.energy, Pixel(0, index))
+}
+
+fun createGraphNodes(listEnergies: List<List<Double>>): List<MutableList<NodeData>> {
+    val graphNodes = List(listEnergies.size) { MutableList(listEnergies[0].size) {NodeData(0.0)} }
+    listEnergies.last().forEachIndexed { col, energy -> graphNodes.last()[col] = NodeData(energy) }
+    for (line in listEnergies.lastIndex - 1 downTo 0) {
+        listEnergies[line].forEachIndexed { col, _ ->
+            graphNodes[line][col] = bestNextPixelFrom(Pixel(line, col), listEnergies, graphNodes)
+        }
+    }
+    return graphNodes
+}
+
+fun bestNextPixelFrom(
+    current: Pixel,
+    listEnergies: List<List<Double>>,
+    graphNodes: List<List<NodeData>>,
+): NodeData {
+    val nextPixels = possibleNextPixel(current, listEnergies)
+    val bestPreviousNode = nextPixels.map {
+        it to graphNodes[it.line][it.col].energy + listEnergies[current.line][current.col]
+    }.minBy { it.second }
+    return NodeData(bestPreviousNode.second, bestPreviousNode.first)
+}
+
+fun possibleNextPixel(current: Pixel, listEnergies: List<List<Double>>): List<Pixel> {
+    val nbLines = listEnergies.size
+    val nbCols = listEnergies.first().size
+    return listOf(
+        Pixel(current.line + 1, current.col - 1),
+        Pixel(current.line + 1, current.col),
+        Pixel(current.line + 1, current.col + 1)
+    ).filter { it.line in 0 until nbLines && it.col in 0 until nbCols}
+}
--- src/test/kotlin/TestSeamProcessing.kt
+++ src/test/kotlin/TestSeamProcessing.kt
@@ -1,12 +1,11 @@
-import com.youyou.backTrackGraph
-import com.youyou.dijkstraEnergyPath
-import com.youyou.generateGraphFindMinSeam
-import com.youyou.model.Pixel
+import com.youyou.createGraphNodes
+import com.youyou.getMinSeam
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 
 class TestSeamProcessing {
     private lateinit var fakeEnergies: List<List<Double>>
+    private lateinit var bigFakeEnergies: List<List<Double>>
 
     @BeforeTest
     fun setUpData() {
@@ -17,27 +16,37 @@ class TestSeamProcessing {
             listOf(94.6, 1.2, 8.1, 27.3, 30.2, 185.0),
             listOf(0.6, 18.2, 81.1, 207.3, 37.2, 15.3)
         )
+        bigFakeEnergies = listOf(
+            listOf(126.0, 173.0, 166.0, 127.0, 163.0, 188.0, 8.0, 62.0, 100.0, 45.0, 99.0, 58.0, 26.0, 35.0, 45.0),
+            listOf(142.0, 166.0, 161.0, 126.0, 146.0, 242.0, 96.0, 50.0, 94.0, 52.0, 70.0, 42.0, 26.0, 33.0, 44.0),
+            listOf(101.0, 97.0, 42.0, 34.0, 59.0, 207.0, 147.0, 42.0, 72.0, 82.0, 68.0, 78.0, 55.0, 81.0, 92.0),
+            listOf(61.0, 48.0, 10.0, 21.0, 76.0, 231.0, 150.0, 46.0, 12.0, 100.0, 62.0, 38.0, 136.0, 189.0, 188.0),
+            listOf(71.0, 53.0, 32.0, 53.0, 250.0, 183.0, 130.0, 77.0, 24.0, 65.0, 62.0, 34.0, 133.0, 150.0, 147.0),
+            listOf(80.0, 76.0, 73.0, 188.0, 255.0, 20.0, 124.0, 84.0, 37.0, 79.0, 86.0, 37.0, 178.0, 223.0, 224.0),
+            listOf(86.0, 94.0, 102.0, 144.0, 182.0, 160.0, 114.0, 84.0, 65.0, 58.0, 82.0, 122.0, 135.0, 79.0, 75.0),
+            listOf(76.0, 77.0, 93.0, 76.0, 187.0, 177.0, 154.0, 54.0, 25.0, 45.0, 64.0, 122.0, 137.0, 117.0, 129.0),
+            listOf(86.0, 84.0, 82.0, 43.0, 38.0, 51.0, 104.0, 67.0, 34.0, 42.0, 40.0, 64.0, 56.0, 33.0, 35.0),
+            listOf(85.0, 83.0, 83.0, 50.0, 33.0, 43.0, 105.0, 65.0, 32.0, 32.0, 22.0, 59.0, 71.0, 43.0, 44.0)
+        )
+    }
+    
+    @Test
+    fun testCreateGraphNodes() {
+        var resultGraph = createGraphNodes(fakeEnergies)
+        resultGraph.forEach { println(it) }
+
+        println("\n======================================\n")
+        resultGraph = createGraphNodes(bigFakeEnergies)
+        resultGraph.forEach { println(it) }
     }
 
     @Test
-    fun testDijkstraEnergyPath() {
-        println("*** Input sample ***")
-        fakeEnergies.forEach { println(it) }
-        println("*** Path found ***")
-        val paths = fakeEnergies.first().indices.map { Pixel(0, it) }
-            .map { backTrackGraph(dijkstraEnergyPath(fakeEnergies, it), fakeEnergies.lastIndex) }
-//        graph.forEach(::println)
-//        val path = backTrackGraph(graph, fakeEnergies.lastIndex)
-        paths.forEach(::println)
-    }
+    fun testGetMinSeam() {
+        val (totalEnergy, path) = getMinSeam(createGraphNodes(fakeEnergies))
+        println("$totalEnergy => $path")
 
-    @Test
-    fun testSeamProcessingV2() {
-        val (minSeamEnergy, seamPath) = generateGraphFindMinSeam(fakeEnergies)
-        println("*** Input sample ***")
-        fakeEnergies.forEach { println(it) }
-        println("*** Path found ***")
-        println("energy: $minSeamEnergy")
-        seamPath.forEach(::println)
+        println("\n======================================\n")
+        val (totalEnergy2, path2) = getMinSeam(createGraphNodes(bigFakeEnergies))
+        println("$totalEnergy2 => $path2")
     }
 }
